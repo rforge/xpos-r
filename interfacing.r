957b46a5b786c3cb8399d0076db3fda0d0e5aa12
oliviercrespo <oliviercrespo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-20 06:58:36 +0000

    pluging in apsim: working until main.r:debugging stop
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xpos-r@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- interfacing.r
+++ interfacing.r
@@ -14,23 +14,23 @@ source("rwfileOp.r");
 decSpe <- apsim_init();
 decS <- decSpe$decS;
 decNam <- decSpe$decNam;
-path2Outputs <- decSpe$path2out;
+path2apsimOutputs <- decSpe$path2out;
 
 ## change decision variables in .sim
-file.copy(paste(path2Outputs,"initFile.sim",sep=""),paste(path2Outputs,"noYearFile.sim",sep=""),overwrite=TRUE);
-changeVar(	decNam[1,1],	decS[2,1],	paste(path2Outputs,"noYearFile.sim",sep=""),paste(path2Outputs,"noYearFile.sim",sep=""));
-changeVar(	decNam[1,2],	decS[2,2],	paste(path2Outputs,"noYearFile.sim",sep=""),paste(path2Outputs,"noYearFile.sim",sep=""));
+file.copy(paste(path2apsimOutputs,"initFile.sim",sep=""),paste(path2apsimOutputs,"noYearFile.sim",sep=""),overwrite=TRUE);
+changeVar(	decNam[1,1],	decS[2,1],	paste(path2apsimOutputs,"noYearFile.sim",sep=""),paste(path2apsimOutputs,"noYearFile.sim",sep=""));
+changeVar(	decNam[1,2],	decS[2,2],	paste(path2apsimOutputs,"noYearFile.sim",sep=""),paste(path2apsimOutputs,"noYearFile.sim",sep=""));
 
 ## change pertubation variables in .sim
 # startYear and endYear define the length of the time period simulated (has to be included in the met file)
-file.copy(paste(path2Outputs,"noYearFile.sim",sep=""),paste(path2Outputs,"fileToSimulate.sim",sep=""),overwrite=TRUE);
-changeVar(	"var_startYear",		"1998",	paste(path2Outputs,"fileToSimulate.sim",sep=""),paste(path2Outputs,"fileToSimulate.sim",sep=""));
-changeVar(	"var_endYear",		"2003",	paste(path2Outputs,"fileToSimulate.sim",sep=""),paste(path2Outputs,"fileToSimulate.sim",sep=""));
+file.copy(paste(path2apsimOutputs,"noYearFile.sim",sep=""),paste(path2apsimOutputs,"fileToSimulate.sim",sep=""),overwrite=TRUE);
+changeVar(	"var_startYear",		"1998",	paste(path2apsimOutputs,"fileToSimulate.sim",sep=""),paste(path2apsimOutputs,"fileToSimulate.sim",sep=""));
+changeVar(	"var_endYear",		"2003",	paste(path2apsimOutputs,"fileToSimulate.sim",sep=""),paste(path2apsimOutputs,"fileToSimulate.sim",sep=""));
 
 ## run simulation for all .sim files
-apsim_simulate(path2Outputs,"fileToSimulate");
+apsim_simulate(path2apsimOutputs,"fileToSimulate");
 
 ## read outputs from .out files
 # take out only the last year results
-temp <- apsim_readOutputs(path2Outputs,"simulation");
+temp <- apsim_readOutputs(path2apsimOutputs,"simulation");
 out <- temp[dim(temp)[1],];
