18eba7216569de873890b84b9e3f8af6ffd24657
apsimcomputer <apsimcomputer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-09 05:15:20 +0000

    jody's comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xpos-r@175 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- optimization/call_xPos-a.r
+++ optimization/call_xPos-a.r
@@ -5,6 +5,16 @@
  # collection of command line to call xPos-a
  ####################################################################
 
+#xPos <- function(
+#			mod,			## model to be simulated for evaluation: Deb test functions{1,2,3,4}, apsim{10}
+#			partNo,		## No of divided parts per region {2,3}
+#			decNo,		## decision number per region {0:automatic, 1...n }
+#			perNo,		## perturbation param number (no of random occurences to test uncertainty)
+#			simLimit,	## simulation number limit
+#			timLimit,	## time limit in sec
+#		  seeItThrough=NULL,	## for graphics {"g","d"}
+#			seed=NULL)## if needed (integer)
+
 source('main.r');
 ## CALL EXAMPLE
 #xPos(mod,partNo,decNo,perNo,simLimit,timLimit,seeItThrough=NULL,seed=NULL)
