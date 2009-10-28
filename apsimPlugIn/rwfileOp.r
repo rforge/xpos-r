393cd55bf58a3658dbeccf3f0d547d51e5bc05bd
oliviercrespo <oliviercrespo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-28 14:49:07 +0000

    shp tools (need package "shapefiles")
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xpos-r@183 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- apsimPlugIn/rwfileOp.r
+++ apsimPlugIn/rwfileOp.r
@@ -15,3 +15,36 @@ changeVar <- function(oldVar,newVar,oldFile,newFile)
 	tempFile <- sub(oldVar, newVar, tempFile);
 	writeLines(tempFile,newFile,sep="\n");
 }
+##
+ # collect index related to a location border set
+ ####################################################################
+ # according to "shapefiles" package
+ ####################################################################
+shpIndex.inLonLat <- function(lonMin,lonMax,latMin,latMax,shapefile)
+{	
+	indices <- NULL;
+	for(i in 1:length(shapefile$shp$shp)){
+		if(shapefile$shp$shp[[i]]$box[1]>lonMin && shapefile$shp$shp[[i]]$box[3]<lonMax){
+		if(shapefile$shp$shp[[i]]$box[2]>latMin && shapefile$shp$shp[[i]]$box[4]<latMax){
+			indices <- c(indices,i);
+		}}
+	}
+	
+return(as.array(indices));
+}
+##
+ # print values for one location index
+ ####################################################################
+ # according to "shapefiles" package
+ ####################################################################
+dbfValues.atIndex <- function(ind,shapefile)
+{	
+	values <- NULL;
+	for(i in 1:length(shapefile$dbf$dbf)){
+		values <- c(values,shapefile$dbf$dbf[[i]][ind]);
+	}
+	
+return(as.array(values));
+}
+
+
