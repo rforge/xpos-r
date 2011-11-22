2c19ef505c345b25c881394d7d1016464f698565
oliviercrespo <oliviercrespo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-22 12:12:13 +0000

    read soils files to get all types
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xpos-r@338 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- DSSATtools/RtestCode.r
+++ DSSATtools/RtestCode.r
@@ -1,26 +1,32 @@
-read_soilFile <- function(path2file)
+read_oneSOLfile <- function(path2file)
 {
 	## find out first part of soil file name
-	temp <- strsplit(path2file,split=".",fixed=TRUE);
+	temp <- strsplit(path2file,split="/",fixed=TRUE);
+	temp <- strsplit(temp[[1]][length(temp[[1]])],split=".",fixed=TRUE);
 	first <- temp[[1]][1];
 	
-	## read all file
-	temp <- scan(path2file,what="character",sep=" ", blank.lines.skip = FALSE);
+	## read all file, line by line
+	temp <- readLines(path2file);
+	soils <- NULL;
 	for(i in 1:length(temp)){
-		test <- strsplit(temp[i],split=first);
-print(test);
-print(!is.character(test))	;
-browser();
-		# if empty character, skip to nect step
-		if(!is.character(test))	next;
-
-		if(test[[1]][1]=="*"){	## this is a soil
-			print(paste(test[[1]][1],first,test[[1]][2]," cmp ",temp[i]));
+		test <- try(strsplit(temp[i],split=first),silent=TRUE);
+		if(length(test[[1]])>1 && test[[1]][1]=="*"){	## this is a soil
+			soils <- rbind(soils,temp[i]);
 		}
 	}
 
-	## i'm looking for "*firstxxxxxxx"
-
-return(temp)
+return(soils);
 }
 
+read_allSOLfiles <- function(path2Dir)
+{
+	SOLfile <- list.files(path2Dir,pattern="*.SOL");
+
+	soils <- NULL;
+	for (i in 1:length(SOLfile)){
+		soils <- rbind(soils,read_oneSOLfile(paste(path2Dir,SOLfile[i],sep="")));
+	}
+
+	write(soils,file=paste(path2Dir,"allSoilstype.tmp",sep=""))
+return(soils);
+}
